c5e1e07caaaec27cb26c89b3ed84bf351af26a57
maheshbghule2000 <111970471+maheshbghule2000@users.noreply.github.com>
2023-01-19 21:44:21 +0530

    Initial commit

---
+++ app/src/main/java/com/example/calculator/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.calculator;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+//    Button mbutton0,mbutton1,mbutton2,mbutton3,mbutton4,mbutton5,mbutton6,mbutton7,mbutton8,mbutton9,mbuttonadd,mbuttonsub,mbuttondiv,mbuttonmul,mbuttondec,mbuttonequal,mbuttonclr,mbuttonsqrt,mbuttonsin,mbuttoncos;
+//    EditText et;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+}
